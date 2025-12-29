0bf1392b43d57a773e388398bc2aec8dac7bea93
Zakhar Saprun <62b2fbbb@gmail.com>
2025-12-29 21:32:58 +0200

    Fix hardcode2

--- task04/main.tf
+++ task04/main.tf
@@ -32,12 +32,12 @@ resource "azurerm_public_ip" "pip" {
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = var.nic_ip_config_name
+  name                = var.nic_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "internal"
+    name                          = var.nic_ip_config_name
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.pip.id
