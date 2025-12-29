ee417cf49016d65db80ecccc615bce24a59f97e8
Zakhar Saprun <62b2fbbb@gmail.com>
2025-12-29 21:18:47 +0200

    Rm old

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vm_public_ip" {
-  description = "The public IP address of the VM"
-  value       = azurerm_public_ip.pip.ip_address
-}
-
-output "vm_fqdn" {
-  description = "The FQDN of the VM"
-  value       = azurerm_public_ip.pip.fqdn
-}
